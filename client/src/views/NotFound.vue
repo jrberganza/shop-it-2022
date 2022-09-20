55e9f91c2f08e3f04e189811c1a7dba16ad525fe
jrberganza <68700999+jrberganza@users.noreply.github.com>
2022-09-19 18:35:16 -0600

    404 eror page

---
+++ client/src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="not-found">
+    <p>No se encontró la página</p>
+    <VBtn @click="() => $router.go(-1)">Regresar</VBtn>
+  </div>
+</template>
+
+<script>
+import { VBtn } from 'vuetify/lib';
+
+export default {
+  name: 'NotFound',
+  data: () => ({}),
+  components: { VBtn },
+};
+</script>
