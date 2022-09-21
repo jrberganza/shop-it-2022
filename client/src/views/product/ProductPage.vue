ae0b20fea60cea3e20e1b49433698691c49db40b
jrberganza <68700999+jrberganza@users.noreply.github.com>
2022-09-21 16:03:22 -0600

    Basic product page

--- client/src/views/product/ProductPage.vue
+++ client/src/views/product/ProductPage.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="product">
-    <p>Producto {{$route.params.id}}</p>
+  <div class="product"><template v-if="product">
+      <h1>{{product.name}}</h1>
+      <p>{{product.price}} - {{product.shopName}}</p>
+      <p>{{product.shortDesc}}</p>
+    </template>
+    <template v-else>
+      <VSkeletonLoader type="card"></VSkeletonLoader>
+    </template>
   </div>
 </template>
 
@@ -9,7 +15,34 @@
 export default {
   name: 'ProductPage',
   props: ['id'],
-  data: () => ({}),
+  data: () => ({
+    /** @type {any | null} */ product: null,
+  }),
+  methods: {
+    getProduct(id) {
+      let apiResponse = [
+        {
+          id: 1,
+          name: 'ola',
+          shopName: 'tienda2',
+          price: 12.34,
+          shortDesc: 'example product 1',
+        },
+        {
+          id: 2,
+          name: 'ola2',
+          shopName: 'tienda2',
+          price: 12.34,
+          shortDesc: 'example product 2',
+        },
+      ]; // example
+
+      return apiResponse[id];
+    }
+  },
+  mounted() {
+    this.product = this.getProduct(this.$route.params.id);
+  },
   components: {},
 };
 </script>
