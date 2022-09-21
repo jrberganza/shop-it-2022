8c7c80337da317aed8b493e2c009d37c47470a81
jrberganza <68700999+jrberganza@users.noreply.github.com>
2022-09-21 11:16:52 -0600

    Create basic shop list component

---
+++ client/src/components/ShopList.vue
@@ -0,0 +1,33 @@
+<template>
+  <VRow>
+    <template v-if="shops.length > 0">
+      <VCol v-for="shop in shops" :key="shop.id" cols="4">
+        <VCard @click="() => $router.push('/shop/' + shop.id)">
+          <img src="../assets/logo.png" width="100%" height="100%" /> <!-- TODO: Change to map -->
+          <VCardTitle>{{shop.name}}</VCardTitle>
+          <VCardSubtitle>{{shop.address}} - {{shop.phoneNumber}}</VCardSubtitle>
+          <VCardText>{{shop.shortDesc}}</VCardText>
+        </VCard>
+      </VCol>
+    </template>
+    <template v-else>
+      <VCol v-for="i in Array(3 * 10)" :key="i" cols="4">
+        <VSkeletonLoader type="card"></VSkeletonLoader>
+      </VCol>
+    </template>
+  </VRow>
+</template>
+
+<script>
+import {
+  VRow, VCol, VSkeletonLoader, VCard, VCardTitle, VCardSubtitle, VCardText, VImg
+} from 'vuetify/lib';
+
+export default {
+  name: 'ShopList',
+  props: ['shops'],
+  components: {
+    VRow, VCol, VSkeletonLoader, VCard, VCardTitle, VCardSubtitle, VCardText, VImg
+  },
+};
+</script>
--- client/src/views/Search.vue
+++ client/src/views/Search.vue
@@ -3,11 +3,11 @@
     <VRow>
       <VCol cols="12" md="6">
         <h1>Products</h1>
-        <ProductList :products="[]"></ProductList>
+        <ProductList :products="products"></ProductList>
       </VCol>
       <VCol cols="12" md="6">
         <h1>Shops</h1>
-        <ProductList :products="[]"></ProductList> <!-- TODO: Change to ShopList -->
+        <ShopList :shops="shops"></ShopList>
       </VCol>
     </VRow>
   </div>
@@ -16,9 +16,44 @@
 <script>
 import { VRow, VCol } from 'vuetify/lib';
 import ProductList from '../components/ProductList.vue';
+import ShopList from '../components/ShopList.vue';
 
 export default {
   name: 'Search',
-  components: { VRow, VCol, ProductList },
+  data: () => ({
+    products: [
+      {
+        id: 1,
+        name: 'ola',
+        shopName: 'tienda2',
+        price: 12.34,
+        shortDesc: 'example product 1',
+      },
+      {
+        id: 2,
+        name: 'ola2',
+        shopName: 'tienda2',
+        price: 12.34,
+        shortDesc: 'example product 2',
+      },
+    ],
+    shops: [
+      {
+        id: 1,
+        name: 'tienda1',
+        address: 'aquí',
+        phoneNumber: '1234-5678',
+        shortDesc: 'example shop 1',
+      },
+      {
+        id: 2,
+        name: 'tienda2',
+        address: 'allá',
+        phoneNumber: '5678-1234',
+        shortDesc: 'example shop 2',
+      },
+    ],
+  }),
+  components: { VRow, VCol, ProductList, ShopList },
 };
 </script>
