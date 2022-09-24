0f3254d511883bdf2efe4f44e2bece943b1f3da4
jrberganza <68700999+jrberganza@users.noreply.github.com>
2022-09-23 21:34:47 -0600

    Prettier shop editor

--- client/src/views/shop/YourShops.vue
+++ client/src/views/shop/YourShops.vue
@@ -2,47 +2,58 @@
   <div class="your-shops">
     <h1>Your shops</h1>
     <VRow>
-      <VCol cols="12" lg="4">
-        <VVirtualScroll :items="shops" :itemHeight="320" height="500" bench="1">
-          <template v-slot:default="{ item }">
-            <VCard class="mx-2" height="300" @click="getShop(item.id)">
-              <img src="@/assets/logo.png" width="100%" height="100" /> <!-- TODO: Change to map -->
-              <VCardTitle>{{item.name}}</VCardTitle>
-              <VCardSubtitle>{{item.address}} - {{item.phoneNumber}}</VCardSubtitle>
-              <VCardText>{{item.shortDesc}}</VCardText>
+      <VCol cols="12" lg="6" order="1" order-lg="12">
+        <template v-if="selectedShop">
+          <VCard>
+            <VImg :src="LogoImg" height="300" /> <!-- TODO: Change to map -->
+            <VCardTitle>
+              <VTextField label="Name" v-model="selectedShop.name"></VTextField>
+            </VCardTitle>
+            <VCardSubtitle>
+              <VTextField label="Address" v-model="selectedShop.address"></VTextField>
+              <VTextField label="Phone Number" v-model="selectedShop.phoneNumber"></VTextField>
+            </VCardSubtitle>
+            <VCardText>
+              <VTextarea label="Description" v-model="selectedShop.desc"></VTextarea>
+            </VCardText>
+            <VCardActions>
+              <VBtn block @click="() => selectedShop = null">
+                <VIcon>mdi-floppy</VIcon> Save
+              </VBtn>
+            </VCardActions>
+          </VCard>
+        </template>
+        <p v-else><em>No shop selected.</em></p>
+      </VCol>
+      <VCol cols="12" lg="6" order="12" order-lg="1">
+        <VDataIterator :items="shops" :itemsPerPage="5">
+          <template v-slot:default="{ items }">
+            <VCard v-for="shop in items" :key="shop.id" class="ma-2" @click="getShop(shop.id)">
+              <VImg :src="LogoImg" height="100" /> <!-- TODO: Change to map -->
+              <VCardTitle>{{shop.name}}</VCardTitle>
+              <VCardSubtitle>{{shop.address}} - {{shop.phoneNumber}}</VCardSubtitle>
+              <VCardText>{{shop.shortDesc}}</VCardText>
             </VCard>
           </template>
-        </VVirtualScroll>
+        </VDataIterator>
         <VBtn block>
           <VIcon>mdi-plus</VIcon> New shop
         </VBtn>
-      </VCol>
-      <VCol cols="12" lg="8">
-        <template v-if="selectedShop">
-          <VForm>
-            <VTextField label="Name" v-model="selectedShop.name"></VTextField>
-            <VTextField label="Address" v-model="selectedShop.address"></VTextField>
-            <VTextField label="Phone Number" v-model="selectedShop.phoneNumber"></VTextField>
-            <VTextField label="Description" v-model="selectedShop.desc"></VTextField>
-            <VBtn block @click="() => selectedShop = null">
-              <VIcon>mdi-floppy</VIcon> Save
-            </VBtn>
-          </VForm>
-        </template>
-        <template v-else><em>No shop selected.</em></template>
       </VCol>
     </VRow>
   </div>
 </template>
 
 <script>
-import { VRow, VCol, VForm, VTextField, VBtn, VVirtualScroll, VCard, VCardTitle, VCardSubtitle, VCardText, VIcon } from 'vuetify/lib';
+import { VRow, VCol, VForm, VTextField, VTextarea, VBtn, VDataIterator, VCard, VCardTitle, VCardSubtitle, VCardText, VCardActions, VIcon, VImg } from 'vuetify/lib';
+import LogoImg from '../../assets/logo.png';
 
 export default {
   name: 'YourShops',
   data: () => ({
     /** @type {any | null} */ selectedShop: null,
-    shops: []
+    shops: [],
+    LogoImg
   }),
   methods: {
     getShops() {
@@ -59,6 +70,6 @@ export default {
   mounted() {
     this.getShops();
   },
-  components: { VRow, VCol, VForm, VTextField, VBtn, VVirtualScroll, VCard, VCardTitle, VCardSubtitle, VCardText, VIcon },
+  components: { VRow, VCol, VForm, VTextField, VBtn, VDataIterator, VCard, VCardTitle, VCardSubtitle, VCardText, VTextarea, VCardActions, VIcon, VImg },
 };
 </script>
