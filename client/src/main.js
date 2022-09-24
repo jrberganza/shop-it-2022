faf790d996c180e0674dd94bff9a3aec0b9a905b
jrberganza <68700999+jrberganza@users.noreply.github.com>
2022-09-24 02:38:16 -0600

    Call session API from frontend

--- client/src/main.js
+++ client/src/main.js
@@ -6,6 +6,11 @@ import store from './store'
 
 Vue.config.productionTip = false;
 
+router.beforeEach(function (to, from, next) {
+  store.dispatch('fetchSession');
+  next();
+});
+
 new Vue({
   router,
   vuetify,
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,15 +1,29 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    session: null
   },
   mutations: {
+    updateSession(state, session) {
+      state.session = session;
+    }
   },
   actions: {
+    async fetchSession({ commit }) {
+      const res = await fetch("/api/session/current.php");
+      const json = await res.json();
+
+      if (json.success) {
+        commit('updateSession', json);
+      } else {
+        commit('updateSession', null);
+      }
+    }
   },
   modules: {
   }
-})
+});
