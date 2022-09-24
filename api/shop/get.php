87c49ebce8b247b128678b378686bad341c24756
jrberganza <68700999+jrberganza@users.noreply.github.com>
2022-09-24 04:44:58 -0600

    Fix shop pages for non-logged in users

--- api/shop/get.php
+++ api/shop/get.php
@@ -6,19 +6,13 @@ require "../private/utils.php";
 
 header('Content-type: application/json');
 
-$session = getCurrentSession($db);
-
 if (!isset($_GET["id"])) {
     resFail("No shop specified");
 }
 $shopId = $_GET["id"];
 
-if (!$session) {
-    resFail("Not logged in");
-}
-
-$stmt = $db->prepare("SELECT shop_id as id, name, address, phone_number as phoneNumber, description, disabled FROM shops WHERE user_id = ? AND shop_id = ?");
-$stmt->bind_param("ii", $session->id, $shopId);
+$stmt = $db->prepare("SELECT shop_id as id, name, address, phone_number as phoneNumber, description, disabled FROM shops WHERE shop_id = ?");
+$stmt->bind_param("i", $shopId);
 $stmt->execute();
 $result = $stmt->get_result();
 
