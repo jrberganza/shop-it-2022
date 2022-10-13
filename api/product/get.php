3b81bb3b15b1396db052d01d5d4a383f38d9ebb5
jrberganza <68700999+jrberganza@users.noreply.github.com>
2022-10-13 10:51:37 -0600

    Public retrieve product

--- api/product/get.php
+++ api/product/get.php
@@ -2,16 +2,53 @@
 
 require '../utils/request.php';
 
-$currId = $_GET['id'];
+$req->useDb();
+$req->useSession();
 
-$product = new \stdClass();
+if (!isset($_GET["id"])) {
+    $req->fail("No product specified");
+}
+$productId = $_GET['id'];
 
-$product->id = $currId;
-$product->name = "Producto " . $currId;
-$product->shopName = "Tienda " . random_int(1, 100);
-$product->price =  random_int(0, 9999) / 100.0;
-$product->desc = "Product description " . $currId;
-$product->rating = random_int(0, 10) / 2.0;
-$product->photos = [];
+$stmt = $req->prepareQuery("SELECT
+    p.product_id as id,
+    p.name as name,
+    p.price as price,
+    p.description as description,
+    p.disabled as disabled,
+    cast(coalesce(r.rating, 0.0) as double) as rating
+FROM
+    products p
+JOIN
+    shops s USING (shop_id)
+LEFT JOIN
+    (SELECT avg(rating) as rating, product_id FROM product_ratings WHERE product_id = @{i:productId}) r USING (product_id)
+WHERE
+    p.disabled = FALSE AND
+    s.disabled = FALSE AND
+    p.product_id = @{i:productId}", [
+    "productId" => $productId,
+]);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$resObj = $result->fetch_object();
+
+if (!$resObj) {
+    $req->fail("No product found");
+}
+
+$stmt = $req->prepareQuery("SELECT product_photo_id FROM product_photos WHERE product_id = @{i:productId}", [
+    "productId" => $productId,
+]);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$resObj->photos = array();
+while ($row = $result->fetch_array()) {
+    array_push($resObj->photos, $row["product_photo_id"]);
+}
+
+$req->success($resObj);
 
 $req->success($product);
