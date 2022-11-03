cce5a18a8c4050f8591ed77057da4acf9629f137
jrberganza <68700999+jrberganza@users.noreply.github.com>
2022-11-03 11:59:01 -0600

    Hide current user in user management

--- api/user/all.php
+++ api/user/all.php
@@ -4,7 +4,9 @@ require_once "../utils/request.php";
 
 $req->requireAdminPrivileges();
 
-$stmt = $req->prepareQuery("SELECT user_id as id, email as email, display_name as displayName, role as role FROM users ORDER BY created_at", []);
+$stmt = $req->prepareQuery("SELECT user_id as id, email as email, display_name as displayName, role as role FROM users WHERE user_id != @{i:userId} ORDER BY created_at", [
+    "userId" => $req->getSession()->id,
+]);
 $stmt->execute();
 $result = $stmt->get_result();
 
