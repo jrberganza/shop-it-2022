ac2a8d8f36a2999a481df4162de8b6983f41b931
jrberganza <68700999+jrberganza@users.noreply.github.com>
2022-11-02 23:09:37 -0600

    Moderation tables

---
+++ db/moderation.sql
@@ -0,0 +1,69 @@
+CREATE TABLE `$moderation$shops` (
+  `shop_id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `zone` int NOT NULL,
+  `municipality_id` int NOT NULL,
+  `latitude` double NOT NULL,
+  `longitude` double NOT NULL,
+  `phone_number` varchar(20) NOT NULL,
+  `description` varchar(512) NOT NULL,
+  `disabled` bool NOT NULL DEFAULT TRUE,
+  `user_id` int NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT (now()),
+  `updated_at` datetime NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE `$moderation$products` (
+  `product_id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `price` double NOT NULL,
+  `description` varchar(512) NOT NULL,
+  `disabled` bool NOT NULL DEFAULT TRUE,
+  `shop_id` int NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT (now()),
+  `updated_at` datetime NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE `$moderation$shop_photo` (
+  `photo_id` int NOT NULL,
+  `shop_id` int NOT NULL,
+  PRIMARY KEY (`photo_id`, `shop_id`)
+);
+
+CREATE TABLE `$moderation$product_photo` (
+  `photo_id` int NOT NULL,
+  `product_id` int NOT NULL,
+  PRIMARY KEY (`photo_id`, `product_id`)
+);
+
+CREATE TABLE `$moderation$comments` (
+  `comment_id` int PRIMARY KEY AUTO_INCREMENT,
+  `shop_id` int,
+  `product_id` int,
+  `author_id` int NOT NULL,
+  `content` varchar(512) NOT NULL,
+  `parent_comment_id` int DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT (now())
+);
+
+ALTER TABLE `$moderation$shops` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+ALTER TABLE `$moderation$shops` ADD FOREIGN KEY (`municipality_id`) REFERENCES `municipalities` (`municipality_id`);
+
+ALTER TABLE `$moderation$products` ADD FOREIGN KEY (`shop_id`) REFERENCES `shops` (`shop_id`);
+
+ALTER TABLE `$moderation$shop_photo` ADD FOREIGN KEY (`photo_id`) REFERENCES `photos` (`photo_id`);
+
+ALTER TABLE `$moderation$shop_photo` ADD FOREIGN KEY (`shop_id`) REFERENCES `shops` (`shop_id`);
+
+ALTER TABLE `$moderation$product_photo` ADD FOREIGN KEY (`photo_id`) REFERENCES `photos` (`photo_id`);
+
+ALTER TABLE `$moderation$product_photo` ADD FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
+
+ALTER TABLE `$moderation$comments` ADD FOREIGN KEY (`shop_id`) REFERENCES `shops` (`shop_id`);
+
+ALTER TABLE `$moderation$comments` ADD FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
+
+ALTER TABLE `$moderation$comments` ADD FOREIGN KEY (`author_id`) REFERENCES `users` (`user_id`);
+
+ALTER TABLE `$moderation$comments` ADD FOREIGN KEY (`parent_comment_id`) REFERENCES `comments` (`comment_id`);
